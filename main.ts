a039362196568108b127cac81135f0ba12f9de9b
1OvO1 <36185370+1OvO1@users.noreply.github.com>
2025-06-07 13:00:19 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,245 @@
+// 常量定义
+const MQTT_BOOL_TYPE_IS_TRUE = true;
+const MQTT_BOOL_TYPE_IS_FALSE = false;
+const MQTT_STR_TYPE_IS_NONE = "";
+
+// 定义最新消息映射的类型
+interface LatestMessagesMap {
+    [topic: string]: string;
+}
+
+// 存储订阅话题的回调函数
+const mqttSubscribeHandlers: { [topic: string]: (message: string) => void } = {};
+
+// 全局变量
+let serialInitialized = MQTT_BOOL_TYPE_IS_FALSE;
+let wifiInitialized = MQTT_BOOL_TYPE_IS_FALSE;
+let mqttInitialized = MQTT_BOOL_TYPE_IS_FALSE;
+let serialTX: SerialPin = SerialPin.P2;
+let serialRX: SerialPin = SerialPin.P1;
+let serialBaudRate = 115200;
+let wifiSSID = MQTT_STR_TYPE_IS_NONE;
+let wifiPassword = MQTT_STR_TYPE_IS_NONE;
+let mqttClientID = MQTT_STR_TYPE_IS_NONE;
+let mqttUsername = MQTT_STR_TYPE_IS_NONE;
+let mqttPassword = MQTT_STR_TYPE_IS_NONE;
+let mqttServer = MQTT_STR_TYPE_IS_NONE;
+let mqttPort = 1883;
+
+// 存储接收到的消息
+let receivedMessage = "";
+
+/**
+ * mqtt implementation method.
+ */
+//% weight=10 color=#008B00 icon="\uf1eb" block="MQTT"
+namespace MQTT {
+
+    /**
+     * 串口初始化
+     * @param receive 接收引脚
+     * @param send 发送引脚
+     * @param baudRate 波特率
+     */
+    //% weight=105
+    //% receive.fieldEditor="gridpicker" receive.fieldOptions.columns=3
+    //% send.fieldEditor="gridpicker" send.fieldOptions.columns=3
+    //% blockId=em_mqtt_serial_init
+    //% block="串口初始化 | 接收数据 RX: %receive| 发送数据 TX: %send | 波特率: %baudRate"
+    //% subcategory="初始化"
+    export function em_mqtt_serial_init(receive: SerialPin, send: SerialPin, baudRate: BaudRate): void {
+        serialTX = send;
+        serialRX = receive;
+        serialBaudRate = baudRate;
+        serial.redirect(
+            serialTX,
+            serialRX,
+            serialBaudRate
+        );
+        serial.setTxBufferSize(128);
+        serial.setRxBufferSize(128);
+        // 简单验证，发送 AT 指令并检查返回结果
+        sendATCommand("AT");
+        basic.pause(500);
+        if (receivedMessage.includes("OK")) {
+            serialInitialized = MQTT_BOOL_TYPE_IS_TRUE;
+            receivedMessage = ""; // 清空消息
+        }
+    }
+
+    /**
+     * 检查串口初始化是否成功
+     */
+    //% blockId=em_mqtt_serial_init_success
+    //% block="串口初始化是否成功"
+    //% weight=104
+    //% subcategory="初始化"
+    export function em_mqtt_serial_init_success(): boolean {
+        return serialInitialized;
+    }
+
+    /**
+     * WiFi 初始化
+     * @param SSID WiFi 名称
+     * @param PASSWORD WiFi 密码
+     */
+    //% weight=103
+    //% blockId=em_mqtt_wifi_init
+    //% block="WiFi 初始化 | 名称: %SSID| 密码: %PASSWORD"
+    //% subcategory="初始化"
+    export function em_mqtt_wifi_init(SSID: string, PASSWORD: string): void {
+        if (!serialInitialized) {
+            em_mqtt_serial_init(serialRX, serialTX, serialBaudRate);
+        }
+        wifiSSID = SSID;
+        wifiPassword = PASSWORD;
+        sendATCommand("AT+CWMODE=1");
+        basic.pause(500);
+        sendATCommand(`AT+CWJAP="${wifiSSID}","${wifiPassword}"`);
+        basic.pause(6000);
+        // 检查是否连接成功
+        sendATCommand("AT+CWJAP?");
+        basic.pause(1000);
+        wifiInitialized = receivedMessage.includes(wifiSSID)
+    }
+
+    /**
+     * 检查 WiFi 初始化是否成功
+     */
+    //% blockId=em_mqtt_wifi_init_success
+    //% block="WiFi 初始化是否成功"
+    //% weight=102
+    //% subcategory="初始化"
+    export function em_mqtt_wifi_init_success(): boolean {
+        return wifiInitialized;
+    }
+
+    /**
+     * MQTT 初始化
+     * @param server 服务器地址
+     * @param port 服务器端口
+     * @param clientId 客户端 ID
+     * @param username 客户端用户名
+     * @param password 客户端密码
+     */
+    //% weight=101
+    //% blockId=em_mqtt_mqtt_init
+    //% block="MQTT 初始化 | 服务器: %server| 端口: %port || 客户端 ID: %clientId | 客户端用户名: %username | 客户端密码: %password"
+    //% subcategory="初始化"
+    export function em_mqtt_mqtt_init(server: string, port: number, clientId: string, username: string, password: string): void {
+        if (!serialInitialized) {
+            em_mqtt_serial_init(serialRX, serialTX, serialBaudRate);
+        }
+        mqttClientID = clientId;
+        mqttUsername = username;
+        mqttPassword = password;
+        mqttServer = server;
+        mqttPort = port;
+        sendATCommand(`AT+MQTTUSERCFG=0,1,"${mqttClientID}","${mqttUsername}","${mqttPassword}",0,0,""`);
+        basic.pause(2000);
+        mqttInitialized = receivedMessage.includes("OK")
+        sendATCommand(`AT+MQTTCONN=0,"${mqttServer}",${mqttPort},1`);
+        basic.pause(2000);
+        mqttInitialized = mqttInitialized && receivedMessage.includes("OK");
+    }
+
+    /**
+     * 检查 MQTT 初始化是否成功
+     */
+    //% blockId=em_mqtt_mqtt_init_success
+    //% block="MQTT 初始化是否成功"
+    //% weight=100
+    //% subcategory="初始化"
+    export function em_mqtt_mqtt_init_success(): boolean {
+        return mqttInitialized;
+    }
+
+    /**
+     * MQTT 订阅话题
+     * @param topic 话题名称
+     * @param qos QoS 等级
+     */
+    //% blockId=mqtt_subscribe
+    //% block="MQTT 订阅话题 %topic|QoS %qos"
+    //% weight=100
+    //% subcategory="MQTT 操作"
+    export function em_mqtt_subscribe(topic: string, qos: number): void {
+        if (!serialInitialized) {
+            em_mqtt_serial_init(serialRX, serialTX, serialBaudRate);
+        }
+        sendATCommand(`AT+MQTTSUB=0,"${topic}",${qos}`);
+        basic.pause(1000);
+    }
+
+    /**
+     * MQTT 订阅话题回调
+     * @param topic 话题名称
+     * @param handler 回调函数
+     */
+    //% blockId=em_mqtt_get_topic_message
+    //% block="MQTT 订阅话题 %topic 收到消息时"
+    //% weight=99
+    //% subcategory="MQTT 操作"
+    export function em_mqtt_get_topic_message(topic: string, handler: (message: string) => void) {
+        if (!serialInitialized) {
+            em_mqtt_serial_init(serialRX, serialTX, serialBaudRate);
+        }
+        mqttSubscribeHandlers[topic] = handler;
+    }
+
+    /**
+     * MQTT 向话题发送数据
+     * @param topic 话题名称
+     * @param data 发送的数据
+     */
+    //% blockId=mqtt_publish_basic
+    //% block="MQTT 向话题(TOPIC) %topic 发送数据 %data"
+    //% weight=98
+    //% subcategory="MQTT 操作"
+    export function em_mqtt_publish_basic(topic: string, data: any): void {
+        if (!serialInitialized) {
+            em_mqtt_serial_init(serialRX, serialTX, serialBaudRate);
+        }
+        sendATCommand(`AT+MQTTPUB=0,"${topic}","${data}",1,0`);
+        basic.pause(200);
+    }
+
+    // 发送 AT 指令
+    function sendATCommand(command: string): void {
+        serial.writeString(command + "\r\n");
+    }
+
+    // 处理接收到的串口数据
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+        receivedMessage += serial.readString();
+
+        // 存储每个topic的最新消息
+        const latestMessages: LatestMessagesMap = {};
+
+        while (receivedMessage.includes("+MQTTSUBRECV")) {
+            const firstIndex = receivedMessage.indexOf("+MQTTSUBRECV");
+            const sencondIndex = receivedMessage.indexOf("+MQTTSUBRECV", firstIndex + 1);
+            let subMessage = receivedMessage.slice(firstIndex, sencondIndex);
+            subMessage = subMessage.split(":").join(",");
+            const parts = subMessage.split(",");
+
+            if (parts.length == 3 && parts[0].includes("+MQTTSUBRECV")) {
+                const topic = parts[1];
+                const message = parts[2];
+                // 只保存每个topic的最新消息
+                latestMessages[topic] = message;
+            }
+
+            receivedMessage = receivedMessage.slice(sencondIndex);
+        }
+
+        // 处理每个topic的最新消息
+        const topics = Object.keys(latestMessages);
+        for (let i = 0; i < topics.length; i++) {
+            const topic = topics[i];
+            if (mqttSubscribeHandlers[topic]) {
+                mqttSubscribeHandlers[topic](latestMessages[topic]);
+            }
+        }
+    });
+}
